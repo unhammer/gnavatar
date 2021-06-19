6a4e61bf66a842bb1f6c17834345ea6ac4766b0f
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2021-06-19 16:23:32 +0200

    init

---
+++ gnavatar-bbdb.el
@@ -0,0 +1,121 @@
+;;; gnavatar-bbdb.el --- Add avatars to BBDB entries -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Kevin Brubeck Unhammer
+
+;; Author: Kevin Brubeck Unhammer <unhammer@fsfe.org>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/unhammer/gnavatar
+;; Keywords: mail, faces, hypermedia
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Helpers for adding image-uri's to BBDB entries from clipboard or
+;;; URLs.  Requires ImageMagick (convert) for resizing.
+
+;;; Interactive functions:
+;;; gnavatar-bbdb-add-clipboard-image-contents
+;;; gnavatar-bbdb-add-clipboard-image-url
+
+;;; Code:
+
+(require 'bbdb)
+(require 'bbdb-com)
+
+(defcustom gnavatar-bbdb-image-size 48
+  "What size to resize the image to before storing in image-uri."
+  :group 'gnavatar
+  :type 'integer)
+
+;;;###autoload
+(defun gnavatar-bbdb-add-clipboard-image-contents (record)
+  "Add image-uri to RECORD using clipboard contents."
+  (interactive
+   (let* ((_ (bbdb-editable))
+          (record (or (bbdb-current-record)
+                      (error "Point not on a record")))
+          (completion-ignore-case t))
+     (list record)))
+  (let* ((file (make-temp-file "clip" nil ".png"))
+         (selection-coding-system 'no-conversion)
+         (coding-system-for-write 'binary))
+    (write-region (or (gui-get-selection 'CLIPBOARD 'image/png)
+                      (error "No image in CLIPBOARD"))
+                  nil file nil 'quiet)
+    (with-temp-buffer
+      (insert-file-contents-literally file)
+      (set-buffer-multibyte nil)
+      (gnavatar-bbdb-add-buffer-image-via-jpg (list record)))))
+
+;;;###autoload
+(defun gnavatar-bbdb-add-clipboard-image-url (record)
+  "Add image-uri field to RECORD using clipboard contents."
+  (interactive
+   (let* ((_ (bbdb-editable))
+          (record (or (bbdb-current-record)
+                      (error "Point not on a record")))
+          (completion-ignore-case t))
+     (list record)))
+  (gnavatar-bbdb-add-image-from-url (list record) (current-kill 0)))
+
+(defvar gnavatar-bbdb-url-timeout 5)
+
+(defun gnavatar-bbdb-add-image-from-url (records url)
+  "Retrieve URL image link, store in image-uri of RECORDS in BBDB."
+  (lexical-let* ((field 'image-uri)
+                 (timeout gnavatar-bbdb-url-timeout))
+    (with-current-buffer (url-retrieve-synchronously url
+                                                     'silent
+                                                     'inhibit-cookies
+                                                     timeout)
+      (goto-char (point-min))
+      (if (and (re-search-forward "^Content-Type: image/\\(.*\\)$" nil 'noerror)
+               (search-forward "\n\n" nil 'noerror))
+          (progn
+            (narrow-to-region (match-end 0) (point-max))
+            (gnavatar-bbdb-add-buffer-image-via-jpg records))
+        (message "Couldn't find image at url %s" url)))))
+
+(defun gnavatar-bbdb-add-buffer-image-via-jpg (records)
+  "Convert image data of current buffer to JPG and add to RECORDS."
+  (shell-command-on-region (point-min) (point-max)
+                           (format "convert -quality 70 -resize %dx - jpeg:-"
+                                   gnavatar-bbdb-image-size)
+                           nil
+                           'replace)
+  (gnavatar-bbdb-add-image-uri records
+                         (buffer-substring-no-properties (point-min) (point-max))))
+
+(defun gnavatar-bbdb-add-image-uri (records img)
+  "Add IMG to each record of RECORDS.
+The image will be base64-encoded and stored in the image-uri field."
+  (let ((field 'image-uri)
+        (img64 (base64-encode-string img 'no-line-break)))
+    (mapcar (lambda (r)
+              (if (bbdb-record-field r field)
+                  (message "%s already has an %s, not setting"
+                           (bbdb-record-field r 'name)
+                           field)
+                (message "Setting %s of %s"
+                         field
+                         (bbdb-record-field r 'name))
+                (bbdb-insert-field r field img64)))
+            records)))
+
+(provide 'gnavatar-bbdb)
+;;; gnavatar-bbdb.el ends here
---
+++ gnavatar.el
@@ -0,0 +1,107 @@
+;;; gnavatar.el --- Gather avatars from various sources -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 Kevin Brubeck Unhammer
+
+;; Author: Kevin Brubeck Unhammer <unhammer@fsfe.org>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; URL: https://github.com/unhammer/gnavatar
+;; Keywords: mail, faces, hypermedia
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Currently just a wrapper around gravatar that adds support for
+;;; BBDB image-uri's; could one day become a general avatar-source.
+
+;;; Meant to be used as a library.
+
+
+;;; Code:
+
+(require 'gravatar)
+(require 'bbdb)
+(require 'bbdb-com)
+
+(defgroup gnavatar nil
+  "Options for gnavatar."
+  :tag "gnavatar"
+  :group 'comm)
+
+(defcustom gnavatar-providers (list #'gravatar-retrieve
+                                    #'gnavatar-bbdb-retrieve)
+  "Prioritised list of avatar providers.
+They should accept the same arguments as `gravatar-retrieve', and
+return the symbol 'error in order to move to the next one in the list."
+  :group 'gnavatar
+  :type '(list function))
+
+(defun gnavatar-bbdb-create-image (record)
+  "Create an image from RECORD, if it has an image-uri."
+  (when-let ((image-uri (bbdb-record-xfield record 'image-uri)))
+    (create-image (base64-decode-string image-uri)
+                  nil                 ; try to guess format
+                  'data)))
+
+(defun gnavatar-bbdb-find-image (mail)
+  "Get image from BBDB record with e-mail MAIL."
+  (when-let ((records (bbdb-search (bbdb-records)
+                                   :mail (regexp-quote mail))))
+    ;; bbdb prefers at most one record per mail, probably no need to
+    ;; support multiple matches
+    (gnavatar-bbdb-create-image (car records))))
+
+(defun gnavatar-bbdb-retrieve (mail-address callback &optional cbargs)
+  "Get image from BBDB record with e-mail MAIL-ADDRESS.
+CALLBACK and CBARGS as in `gravatar-retrieve'."
+  (if-let ((image (gnavatar-bbdb-find-image mail-address)))
+      (apply callback image cbargs)
+    'error))
+
+(defun gnavatar-work (result search-term providers callback &optional cbargs)
+  "Callback wrapper.
+If RESULT is 'error, we try the next of PROVIDERS with
+SEARCH-TERM, else call CALLBACK with RESULT and CBARGS.  We wrap
+the callback to the provider so it'll try the next one if that
+one doesn't work."
+  (if (eq result 'error)
+      (if providers
+          (apply (car providers)        ; gravatar-retrieve
+                 search-term            ; mail-address
+                 (list
+                  (lambda (i) (gnavatar-work i search-term (cdr providers) callback cbargs))))
+        result)
+    (apply callback result cbargs)))
+
+;;;###autoload
+(defun gnavatar-retrieve (mail-address callback &optional cbargs)
+  "Asynchronously retrieve an avatar for MAIL-ADDRESS.
+When finished, call CALLBACK as (apply CALLBACK AVATAR CBARGS),
+where AVATAR is either an image descriptor, or the symbol
+`error' if the retrieval failed.
+
+Note: Providers may change the current buffer, so use
+`with-current-buffer' to insert into the buffer you're in when
+calling this."
+  (gnavatar-work 'error ; we haven't got a result yet, so try the next
+                 mail-address
+                 gnavatar-providers
+                 callback
+                 cbargs))
+
+(provide 'gnavatar)
+;;; gnavatar.el ends here
